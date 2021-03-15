fc8805fe9eee13301ebe81c7bbca46fc0c718054
Hankin72 <tracyguoji@qq.com>
2021-03-15 16:29:05 +0800

    configuration

---
+++ cmake-build-debug/CMakeFiles/COMP9024.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/guohaojin/Documents/COMP9024/Codes/data_type.c" "/Users/guohaojin/Documents/COMP9024/cmake-build-debug/CMakeFiles/COMP9024.dir/Codes/data_type.c.o"
+  "/Users/guohaojin/Documents/COMP9024/Codes/main.c" "/Users/guohaojin/Documents/COMP9024/cmake-build-debug/CMakeFiles/COMP9024.dir/Codes/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/COMP9024.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMP9024.dir/Codes/data_type.c.o"
+  "CMakeFiles/COMP9024.dir/Codes/main.c.o"
+  "COMP9024"
+  "COMP9024.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/COMP9024.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
