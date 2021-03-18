15205ed987202da9f02179954474989ab603e03d
Hankin72 <tracyguoji@qq.com>
2021-03-19 07:30:37 +0800

    asdsa

--- Codes/practice.c
+++ Codes/practice.c
@@ -6,20 +6,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <regex.h>
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
-
-
-void bubble_sort(int arr[], int len) {
-    int i, j, temp;
-    for (i = 0; i < len - 1; i++)
-        for (j = 0; j < len - 1 - i; j++)
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-}
 
 
 static int SelectionSort(int *arr, int size) {
@@ -35,200 +25,173 @@ static int SelectionSort(int *arr, int size) {
         arr[k] = arr[i];
         arr[i] = tmp;
     }
+    return *arr;
 }
 
-//int main() {
-////    int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
-////
-////    int len = (int) sizeof(arr) / sizeof(*arr);
-////    SelectionSort(arr, len);
-////    int i;
-////    for (i = 0; i < len; i++)
-////        printf("%d ", arr[i]);
-////    printf("\n");
-////
-////
 
-int arraydiff(int *A, int max, int len) {
-    int arrayflag[max + 1];
-    int i, j;
-    //初始化标志数组
-    for (i = 0; i <= max; i++) {
-        arrayflag[i] = false;
+// function used to compile the regrex and execute it
+static bool regexMatch(char * str, char * regexPattern) {
+    regex_t reg_check;
+    int output = false;
+    int cflags;
+    cflags = REG_EXTENDED ;
+
+    // compile the regex
+    if(regcomp(&reg_check, regexPattern, cflags)){
+        return false;
     }
-    //剔除算法
-    for (i = 0; i < len; i++) {
-        arrayflag[A[i]] = A[i];
+    // execute the regex
+    if(regexec(&reg_check, str, 0, NULL, 0)) {
+        output = false;
+    }else{
+        output = true;
     }
-    //取出有效数
-    for (i = 0, j = 0; i <= max; i++) {
-        if (arrayflag[i] != false) {
-            A[j++] = arrayflag[i];
+    // free the regex
+    regfree(&reg_check);
+    return output;
+}
+
+
+
+
+static int intset_2_cstring(char *result, int *a, int allocate_len){
+    int index;
+    char left[10]="{";
+    char right[10] ="}";
+    char comma[10] =",";
+    char temp_str[32];
+    index = a[0];
+
+    if (index== 0){
+        strcat(result, left);
+        strcat(result, right);
+
+    }
+    if (index > 0) {
+        strcat(result, left);
+        sprintf(temp_str, "%d", a[1]);
+        strcat(result, temp_str);
+        for (int i = 1; i < index; i++) {
+            strcat(result, comma);
+            sprintf(temp_str, "%d", a[i + 1]);
+            if (strlen(result) > allocate_len - 34) {
+                allocate_len = allocate_len + allocate_len;
+                result = (char *) realloc(result, sizeof(char) * allocate_len);
+            }
+            strcat(result, temp_str);
         }
-    }
-    return j;
-}
-
-int cmp(const void *a, const void *b) //排序规则
-{
-    return *(int *) a - *(int *) b;
-}
-
-
-static int split_String(char str[], char delims[]) {
-
-    char *result = NULL;
-
-    int allocate_len = 100;
-    int *array;
-    int index = 0;
-
-
-    array = (int *) malloc(sizeof(int) * allocate_len);
-    result = strtok(str, delims);
-
-    while (result != NULL) {
-        if (index == allocate_len) {
-            allocate_len = allocate_len * 2;
-            array = (int *) realloc(array, sizeof(int) * allocate_len);
+        if (strlen(result) > allocate_len - 34) {
+            allocate_len = allocate_len + allocate_len;
+            result = (char *) realloc(result, sizeof(char) * allocate_len);
         }
-        array[index] = atoi(result);
-        result = strtok(NULL, delims);
-        index++;
+        strcat(result, right);
     }
-
-    int m, max;
-    max = array[0];
-
-    for (m = 1; m < index; m++) {
-        if (max < array[m])
-            max = array[m];
-    }
-
-    printf("最大元素为 %d\n", max);
-
-
-    int *arrayflag;
-    int i, index_element_num;
-    //初始化标志数组
-
-    arrayflag = malloc(sizeof(int) * (max + 1));
-
-    for (i = 0; i <= max; i++) {
-        arrayflag[i] = false;
-    }
-    //剔除算法
-    for (i = 0; i < index; i++) {
-        arrayflag[array[i]] = array[i];
-    }
-    //取出有效数
-    for (i = 0, index_element_num = 0; i <= max; i++) {
-        if (arrayflag[i] != false) {
-            array[index_element_num++] = arrayflag[i];
-        }
-    }
-    printf("%d\n", index_element_num);
-
-
-    SelectionSort(array, index_element_num);
-    for (int i = 0; i < index_element_num; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    int aa;
-    aa = sizeof(array) / sizeof(array[0]);
-    printf("数组array元素个数为：%d\n", aa);
-
-
-    free(arrayflag);
-    free(array);
+    return *result;
 
 }
-
 
 int main() {
 
-    int a[10] = {9,1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int b[7] = {6,1, 2, 3,11,22,13};
+
+    char a[1024]="{0}";
+
+    int b[]={0};
 
 
-    int count_left;
-    int count_right;
-    int i,j;
-    int flag2,index2 = 0;
-    int flag,index = 0;
-    int *temp;
-    int *temp2;
-    int *temp_total;
-    int allocate_len=100;
-    int index_total=0;
-    count_left =a[0];
-    count_right=b[0];
-    temp2= (int *)malloc(sizeof(int) * allocate_len);
-    for(i = 0;i < count_right;i++)
-    {
-        flag2 = 1;
-        for(j = 0;j < count_left;j++)
-        {
-            if(b[i+1] == a[j+1])
-                flag2 = 0;
+    int allocate_len = 100;
+    int *array_intset;
+    int arrayflag[100];
+    int max;
+    int m, index_element_num;
+    int i;
+    int index = 0;
+
+
+    const char delims[10] = "{ ,}";
+    char *temp_result = NULL;
+
+    array_intset = (int *) malloc(sizeof(int) * allocate_len);
+    temp_result = strtok(a, delims);
+    while (temp_result != NULL) {
+        if (index >= allocate_len) {
+            allocate_len = 2 * allocate_len;
+            array_intset = (int *) realloc(array_intset, sizeof(int) * allocate_len);
         }
-        if(flag2)
-        {
-            temp2[index2] = b[i+1];
-            index2++;
-        }
+        // string to number
+        array_intset[index] = atoi(temp_result);
+        temp_result = strtok(NULL, delims);
+        index++;
     }
 
-    temp = (int *)malloc(sizeof(int) * allocate_len);
-    for(i = 0; i < count_left; i++) {
-        flag = 1;
-        for (j = 0; j < count_right; j++) {
-            if (a[i + 1] == b[j + 1]) {
-                flag = 0;
+//    printf("%d\n",index);
+//    printf("%d\n",array_intset[0]);
+////    printf("%d\n",array_intset[0]);
+
+    if (index >1 ){
+        max = array_intset[0];
+        for (m = 1; m < index; m++) {
+            if (max < array_intset[m]) {
+                max = array_intset[m];
             }
         }
-        if (flag) {
-            if (index >= allocate_len) {
-                allocate_len = allocate_len + allocate_len;
-                temp = (int *) realloc(temp, sizeof(int) * allocate_len);
+
+        // delete the duplicated elements(number)
+        for ( i = 0; i <= max; i++) {
+            arrayflag[i] = false;
+        }
+        //delete Algorithm
+        for ( i = 0; i < index; i++) {
+            arrayflag[array_intset[i]] = array_intset[i];
+        }
+        //Take out the effective number
+        for (i = 0, index_element_num = 0; i <= max; i++) {
+            if (arrayflag[i] != false) {
+                array_intset[index_element_num++] = arrayflag[i];
             }
-            temp[index] = a[i + 1];
-            index++;
         }
+        index = index_element_num;
+        SelectionSort(array_intset,index);
     }
 
-    // index_total
-    temp_total = (int *)malloc(sizeof(int) * allocate_len);
-    for (int m=0; m<index; m++){
-        if(temp_total>=allocate_len){
-            allocate_len = allocate_len * 1.5 ;
-            temp_total =(int*)realloc(temp_total, sizeof(int)*allocate_len);
-        }
-        temp_total[index_total] = temp[m];
-        index_total++;
+
+    int result[100];
+
+    result[0]= index;
+
+    for (int j = 0; j < index+1 ; j++) {
+        result[j + 1] = array_intset[j];
     }
-    for(int n=0; n<index2; n++){
-        if(temp_total>=allocate_len){
-            allocate_len = allocate_len * 1.5 ;
-            temp_total =(int*)realloc(temp_total, sizeof(int)*allocate_len);
-        }
-        temp_total[index_total] = temp2[n];
-        index_total++;
+    printf("----------");
+    printf("%d\n",index);
+
+    printf("%d\n",result[0]);
+    printf("%d\n",result[1]);
+
+
+
+    char output[100];
+    int abc[50] = {1, 0};
+    intset_2_cstring(output,result, allocate_len);
+    printf("%s\n", output);
+
+///////////////////////////
+
+    char *str = "{0} ";
+    char *str2 ="  {100000 , 00199 ,1,2, ,    1000,0}   ";
+
+    int match_result ;
+
+    char *re =  "([ ])*\\{([ ])*\\}([ ])*|([ ])*\\{([ ])*((([ ])*([0-9]*)([ ])*)\\,([ ])*)*(([ ])*(([ ])*([0-9]*)([ ])*))\\}([ ])*";
+
+    match_result = regexMatch(str,re);
+    if (match_result == false){
+        printf("false\n");
+    }else{
+        printf("true\n");
     }
-    SelectionSort(temp_total, index_total);
-
-    printf("-----%d\n",index_total);
-    for(i = 0;i < index_total;i++)
-        printf("%d ",temp_total[i]);
-    printf("\n");
 
 
-    free(temp);
-    free(temp2);
-    free(temp_total);
-
+    free(array_intset);
 
     return 0;
 
